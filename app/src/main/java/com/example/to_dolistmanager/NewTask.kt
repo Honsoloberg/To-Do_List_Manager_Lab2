1c580eb376a0bb4bba10b3e500f29540398ee07d
RivkaRSagi <rivka.r.sagi@gmail.com>
2025-09-18 15:38:05 -0400

    added textView and datepicker

--- app/src/main/java/com/example/to_dolistmanager/NewTask.kt
+++ app/src/main/java/com/example/to_dolistmanager/NewTask.kt
@@ -5,16 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import android.widget.LinearLayout
 
 class NewTask : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_new_task)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_new_task);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.NewTaskLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val screen = findViewById<LinearLayout>(R.id.NewTaskLayout);
+        val textViewTitle = findViewById<TextView>(R.id.taskTitle);
     }
 }
