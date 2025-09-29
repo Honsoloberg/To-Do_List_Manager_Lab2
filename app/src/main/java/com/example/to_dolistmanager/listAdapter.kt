625b05a40ea2b61e9f9d75ff6e2ca73000dd8e91
Logan Butler <logan.butler@ontariotechu.net>
2025-09-29 11:13:02 -0400

    search bar functionality. Currently commented out

--- app/src/main/java/com/example/to_dolistmanager/listAdapter.kt
+++ app/src/main/java/com/example/to_dolistmanager/listAdapter.kt
@@ -6,12 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.CheckBox
+import android.widget.Filter
 import android.widget.TextView
-import androidx.appcompat.view.menu.MenuView
-import androidx.constraintlayout.widget.Group
-import androidx.recyclerview.widget.RecyclerView
+import java.util.Locale
 
-class listAdapter(private val dataset: ArrayList<*>, mContext: Context): ArrayAdapter<Any?>(mContext, R.layout.task, dataset) {
+
+class listAdapter(private val dataset: ArrayList<taskItem>, mContext: Context): ArrayAdapter<Any?>(mContext, R.layout.task, dataset) {
+
+    private var listOriginal: ArrayList<taskItem> = dataset
+    private var listDisplayed: ArrayList<taskItem> = dataset
+//    var inflater: LayoutInflater?
+
+
     private class ItemHolder {
         lateinit var taskName: TextView
         lateinit var checkBox: CheckBox
@@ -20,11 +26,11 @@ class listAdapter(private val dataset: ArrayList<*>, mContext: Context): ArrayAd
     }
 
     override fun getCount(): Int {
-        return dataset.size
+        return listDisplayed.size
     }
 
     override fun getItem(position: Int): taskItem {
-        return dataset[position] as taskItem
+        return listOriginal[position] as taskItem
     }
 
     override fun getView(position: Int, converter: View?, parent: ViewGroup): View{
@@ -46,13 +52,66 @@ class listAdapter(private val dataset: ArrayList<*>, mContext: Context): ArrayAd
             itemHolder = converter.tag as ItemHolder
             result = converter
         }
-
-        val item: taskItem = getItem(position)
-        itemHolder.taskName.text = item.name
-        itemHolder.taskDate.text = item.date
-        itemHolder.taskDesc.text = item.description
-        itemHolder.checkBox.isChecked = item.checked
-
+            val item: taskItem = getItem(position)
+            itemHolder.taskName.text = item.name
+            itemHolder.taskDate.text = item.date
+            itemHolder.taskDesc.text = item.description
+            itemHolder.checkBox.isChecked = item.checked
         return result
     }
+
+//    override fun getFilter(): Filter {
+//        val filter: Filter = object : Filter() {
+//            protected override fun publishResults(
+//                constraint: CharSequence?,
+//                results: FilterResults
+//            ) {
+//                mDisplayed = results.values as ArrayList<taskItem> // has the filtered values
+//                notifyDataSetChanged() // notifies the data with new filtered values
+//            }
+//
+//            override fun performFiltering(constraint: CharSequence?): FilterResults {
+//                var constraint = constraint
+//                val results: FilterResults =
+//                    FilterResults() // Holds the results of a filtering operation in values
+//                val FilteredArrList: ArrayList<taskItem?> = ArrayList<taskItem?>()
+//
+//                if (mOriginal == null) {
+//                    mOriginalValues =
+//                        ArrayList<Product?>(mDisplayedValues) // saves the original data in mOriginalValues
+//                }
+//
+//                /********
+//                 *
+//                 * If constraint(CharSequence that is received) is null returns the mOriginalValues(Original) values
+//                 * else does the Filtering and returns FilteredArrList(Filtered)
+//                 *
+//                 */
+//                if (constraint == null || constraint.length == 0) {
+//                    // set the Original result to return
+//
+//                    results.count = mOriginalValues.size()
+//                    results.values = mOriginalValues
+//                } else {
+//                    constraint = constraint.toString().lowercase(Locale.getDefault())
+//                    for (i in 0..<mOriginalValues.size()) {
+//                        val data: String = mOriginalValues.get(i).name
+//                        if (data.lowercase(Locale.getDefault()).startsWith(constraint.toString())) {
+//                            FilteredArrList.add(
+//                                Product(
+//                                    mOriginalValues.get(i).name,
+//                                    mOriginalValues.get(i).price
+//                                )
+//                            )
+//                        }
+//                    }
+//                    // set the Filtered result to return
+//                    results.count = FilteredArrList.size
+//                    results.values = FilteredArrList
+//                }
+//                return results
+//            }
+//        }
+//        return filter
+//    }
 }
