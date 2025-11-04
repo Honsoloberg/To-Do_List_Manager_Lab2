ed85c92161c598d6b56209a2e44aea9c66231e20
Logan Butler <logan.butler@ontariotechu.net>
2025-11-03 20:39:16 -0500

    taskItem variable update

--- app/src/main/java/com/example/to_dolistmanager/taskItem.kt
+++ app/src/main/java/com/example/to_dolistmanager/taskItem.kt
@@ -1,3 +1,3 @@
 package com.example.to_dolistmanager
 
-class taskItem internal constructor(var name: String?, var date: String?, var description: String?, var checked: Boolean, var color: Int = android.graphics.Color.LTGRAY, var id: Int) {}
+class taskItem internal constructor(var name: String?, var date: String?, var description: String?, var checked: Boolean, var color: Int = android.graphics.Color.LTGRAY, var image: String = "", var id: Int) {}
